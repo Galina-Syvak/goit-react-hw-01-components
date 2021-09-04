fab28246de59cfbe80d58c995b27244bbaf9e6a2
Galina-Syvak <77681699+Galina-Syvak@users.noreply.github.com>
2021-09-05 00:49:50 +0300

    HW base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Profile from './components/Profile/Profile';
+import user from './user.json';
+import Statistics from './components/Statistics/Statistics';
+import data from './statistical-data.json';
+import FriendList from './components/FriendList/FriendList';
+import friends from './friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transaction from './transactions.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title={data.title} stats={data} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transaction} />
     </div>
   );
 }
